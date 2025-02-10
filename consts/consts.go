69fea0e87c46201ece1a01595549e8d70024d102
techmovie <v.abed@icloud.com>
2025-02-10 20:40:28 +0800

    fix: update epg

--- consts/consts.go
+++ consts/consts.go
@@ -2,6 +2,6 @@ package consts
 
 const (
 	UA          = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-	TIME_FORMAT = "200601021504 +0000"
+	TIME_FORMAT = "20060102150405 +0000"
 	THETV_URL   = "https://thetvapp.to"
 )
